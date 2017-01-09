990c15773dd13636d664eb7638943daf1e103c7c
Gael Rottier <gaelrottier@gmail.com>
2017-01-09 15:32:38 +0100

    Texte barré si tache validée

--- src/TodoListContainer/Todo/Todo.js
+++ src/TodoListContainer/Todo/Todo.js
@@ -1,14 +1,32 @@
 import React, {Component} from "react";
-import './Todo.css';
+import "./Todo.css";
 
 export class Todo extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+
+        this.state = {
+            style: {
+                textDecoration: props.todo.completed ? 'line-through' : 'none'
+            }
+        };
     }
 
     handleClick() {
         this.props.onClick(this.props.todo);
+    }
+
+    shouldComponentUpdate() {
+        return !!this.props.todo.completed;
+    }
+
+    componentWillUpdate() {
+        if (this.state.style.textDecoration === 'none') {
+            const style = this.state.style;
+            style.textDecoration = 'line-through';
+            this.setState({style,});
+        }
     }
 
     render() {
@@ -16,8 +34,8 @@ export class Todo extends Component {
 
         return (
             <article className="todo">
-                <span className="todo-title">{todo.title}</span>
-                <button className="btn-check-todo"  onClick={this.handleClick}>Finir</button>
+                <span className="todo-title" style={this.state.style}>{todo.title}</span>
+                <button className="btn-check-todo" onClick={this.handleClick}>Finir</button>
             </article>
         );
     }
